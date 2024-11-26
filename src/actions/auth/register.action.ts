7c0bc63bb28ee5503c251dc89333be782ef86aa1
Abaranasia <rankirlian@gmail.com>
2024-11-26 20:37:19 +0100

    Update to store the cookie if remember_me is active (or delete it if not)

--- src/actions/auth/register.action.ts
+++ src/actions/auth/register.action.ts
@@ -9,8 +9,22 @@ export const registerUser = defineAction({
     password: z.string().min(6),
     remember_me: z.boolean().optional(),
   }),
-  handler: async ({ name, email, password, remember_me }) => {
-    console.log('cosos :>> ', name, email, password, remember_me);
-    return true;
+  handler: async (input, context) => {
+    const { name, email, password, remember_me } = input;
+    const { cookies } = context;
+    
+    console.log("cosos :>> ", name, email, password, remember_me);
+
+    if (remember_me) {
+        cookies.set('email', email, {
+            expires: new Date(Date.now() + 1000 *60 *60 *24 *365), // 1 year
+            path: '/'
+        })
+    } else {
+        cookies.delete('email', {
+            path:'/',
+        });
+    }
+    return {ok: true, msg: 'usuario creado'};
   },
 });
