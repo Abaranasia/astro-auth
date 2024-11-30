10de935be7bae0e36a91eeee58adbda5ab7b6799
Abaranasia <rankirlian@gmail.com>
2024-11-30 21:13:15 +0100

    Update to protect routes from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,17 @@
 import { defineMiddleware } from "astro:middleware";
 import { firebase } from "./firebase/fb-config";
 
+const privateRoutes = ["/protected"];
+const notAuthenticatedRoutes = ['/login', '/register']
+
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware((context, next) => {
   console.log("Middleware executed");
+  const { locals, url, redirect } = context;
+
   const user = firebase.auth.currentUser;
   const isLoggedIn = !!user;
 
-  const { locals } = context;
   locals.isLoggedIn = isLoggedIn; // locals.isLoggedIn has to be declared as intercade in env.d
 
   if (user) { // This logic avoid adding it to protected page
@@ -17,6 +21,16 @@ export const onRequest = defineMiddleware((context, next) => {
       avatar: user.photoURL ?? "",
       emailVerified: user.emailVerified,
     };
+  }
+
+  // This condition disallow the access to protected routes if user is not logged in
+  if (!isLoggedIn && privateRoutes.includes(url.pathname) ) {
+    return redirect('/')
+  }
+
+  // This condition disallow the access to no authenticated routes if user is logged in (to avoid login twice)
+  if (isLoggedIn && notAuthenticatedRoutes.includes(url.pathname) ) {
+    return redirect('/')
   }
 
   return next();
