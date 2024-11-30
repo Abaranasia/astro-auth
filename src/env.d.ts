6ac81fe4bef47a5afac22fd41397a4bf479a5667
Abaranasia <rankirlian@gmail.com>
2024-11-30 20:39:48 +0100

    Add user value to locals to make it app scope available

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,15 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
+interface user {
+    email: string;
+    name: string;
+    avatar: string;
+    emailVerified: boolean;
+}
 declare namespace App {
     interface Locals {
         isLoggedIn: boolean;
+        user: User | null;
     }
 }
