1f8a43dd34e1149d58503f0e582e30a331ae1cac
Abaranasia <rankirlian@gmail.com>
2024-11-30 20:38:14 +0100

    Add a boolean value to make it available in whole app to condition nabar options navigation

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,14 @@
 import { defineMiddleware } from 'astro:middleware';
+import { firebase } from './firebase/fb-config';
 
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware((context, next) => {
   console.log ('Middleware executed');
+  const user = firebase.auth.currentUser;
+  const isLoggedIn = !!user;
+
+  context.locals.isLoggedIn = isLoggedIn; // locals.isLoggedIn has to be declared as intercade in env.d
+
 
   return next()
 });
