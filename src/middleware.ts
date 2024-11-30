b6cf46e9d6c93e1a7e2ea32d4fd31394d21f8ee8
Abaranasia <rankirlian@gmail.com>
2024-11-30 20:52:57 +0100

    Update to expose and adapt user data to whole app

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,23 @@
-import { defineMiddleware } from 'astro:middleware';
-import { firebase } from './firebase/fb-config';
+import { defineMiddleware } from "astro:middleware";
+import { firebase } from "./firebase/fb-config";
 
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware((context, next) => {
-  console.log ('Middleware executed');
+  console.log("Middleware executed");
   const user = firebase.auth.currentUser;
   const isLoggedIn = !!user;
 
-  context.locals.isLoggedIn = isLoggedIn; // locals.isLoggedIn has to be declared as intercade in env.d
+  const { locals } = context;
+  locals.isLoggedIn = isLoggedIn; // locals.isLoggedIn has to be declared as intercade in env.d
 
+  if (user) { // This logic avoid adding it to protected page
+    locals.user = {
+      email: user.email!,
+      name: user.displayName!,
+      avatar: user.photoURL ?? "",
+      emailVerified: user.emailVerified,
+    };
+  }
 
-  return next()
+  return next();
 });
